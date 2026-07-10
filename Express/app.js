648632763413d404a2923c2bf77441f39b66f021
Rocio Arenillas (Chio) <arenillasr@gmail.com>
2026-07-10 16:19:29 +0200

    new sql

--- Express/app.js
+++ Express/app.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser')
 
 //const expressHbs = require('express-handlebars') // Para usar Handlebars
 
+const errorController = require('./controllers/error')
+const db = require('./util/database')
+
 const app = express()
 
 //Si usas Hanslebars
@@ -19,7 +22,13 @@ app.set('views', 'views') //el primer views es el nombre de la carpeta "views"
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
-const errorController = require('./controllers/error')
+db.execute('SELECT * FROM products')
+    .then(result => {
+        console.log(result[0], result[1])
+    })
+    .catch(err => {
+        console.log(err)
+    })
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
