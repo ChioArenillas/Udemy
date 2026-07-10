3c55b400e58faa5a04f01ec8782c959968474cdf
Rocio Arenillas (Chio) <arenillasr@gmail.com>
2026-07-10 10:11:32 +0200

    new

--- Express/routes/admin.js
+++ Express/routes/admin.js
@@ -17,5 +17,10 @@ router.post('/add-product',adminController.postAddProduct)
 //  /admin/products => GET
 router.get('/products', adminController.getAdminProducts)
 
+router.get('/edit-product/:productId', adminController.getEditProduct)
+router.post('/edit-product', adminController.postEditProduct)
+
+router.post('/delete-product', adminController.postDeleteProduct)
+
 
 module.exports = router
