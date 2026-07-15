cc6fe8358f96076cd9bbe4bf6ee1f58ff886459c
Rocio Arenillas (Chio) <arenillasr@gmail.com>
2026-07-15 16:23:33 +0200

    new

--- MongoDB/util/database.js
+++ MongoDB/util/database.js
@@ -1,8 +1,17 @@
-const Sequelize = require('sequelize')
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
 
-const sequelize = new Sequelize('node-complete', 'root', '123456', {
-    dialect: 'mysql',
-    host: 'localhost'
-})
+const mongoConnect = callback => {
+    MongoClient.connect(
+        'mongodb+srv://timi_otull:<FYmtnoDq7GzR7YwN>@cluster0.oyi0bol.mongodb.net/'
+    )
+        .then(client => {
+            console.log('Connected!')
+            callback(client)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+}
 
-module.exports = sequelize
+module.exports = mongoConnect
