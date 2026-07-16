25a5fa029febf181073f3cac29bb531cc35db274
Rocio Arenillas (Chio) <arenillasr@gmail.com>
2026-07-16 14:01:33 +0200

    new

--- MongoDB/routes/admin.js
+++ MongoDB/routes/admin.js
@@ -1,9 +1,7 @@
-const path = require('path')
 const express = require('express')
 
 const  adminController = require('../controllers/admin')
 
-//const rootDir = require('../util/path') 
 
 const router = express.Router()
 
