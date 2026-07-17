4c559af47a2221f8ec7bb60deeb56afee7bbae32
Rocio Arenillas (Chio) <arenillasr@gmail.com>
2026-07-17 12:49:54 +0200

    new

--- MongoDB/controllers/admin.js
+++ MongoDB/controllers/admin.js
@@ -1,4 +1,7 @@
+const mongodb = require('mongodb')
 const Product = require('../models/product')
+
+const ObjectId = mongodb.ObjectId
 
 exports.getAddProduct = (req, res, next) => {
     res.render('admin/edit-product', {
@@ -9,8 +12,7 @@ exports.getAddProduct = (req, res, next) => {
 }
 exports.getAdminProducts = (req, res, next) => {
     
-    req.user
-    .getProducts()
+    Product.fetchAll()
     .then(products => {
     res.render('admin/products', {
         prods: products, 
@@ -42,51 +44,49 @@ exports.postAddProduct = (req, res, next) => {
 
 }
 
-// exports.getEditProduct = (req, res, next) => {
-//     const editMode = req.query.edit
-//     if(!editMode){
-//        return res.redirect('/')
-//     }
-//     const prodId = req.params.productId
-//     req.user
-//         .getProducts({where: {id: prodId}})
-//     .then(products => {
-//         const product = products[0]
-//         if(!product){
-//             return res.redirect('/')
-//         }
-//         res.render('admin/edit-product', {
-//             pageTitle: 'Edit Product', 
-//             path: '/admin/edit-product',
-//             editing: editMode,
-//             product: product
-//         }) 
-//     })
-//     .catch(err => {
-//         console.log(err)
-//     })
-// }
-// exports.postEditProduct = (req, res, next) => {
-//     const prodId = req.body.productId
-//     const updatedTitle = req.body.title;
-//     const updatedImageUrl = req.body.imageUrl;
-//     const updatedPrice = req.body.price;
-//     const updatedDescription = req.body.description
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit
+    if(!editMode){
+       return res.redirect('/')
+    }
+    const prodId = req.params.productId
+    Product.findById(prodId)
+    .then(product => {
+        if(!product){
+            return res.redirect('/')
+        }
+        res.render('admin/edit-product', {
+            pageTitle: 'Edit Product', 
+            path: '/admin/edit-product',
+            editing: editMode,
+            product: product
+        }) 
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
+exports.postEditProduct = (req, res, next) => {
+    const prodId = req.body.productId
+    const updatedTitle = req.body.title;
+    const updatedImageUrl = req.body.imageUrl;
+    const updatedPrice = req.body.price;
+    const updatedDescription = req.body.description
 
-//     Product.findByPk(prodId)
-//     .then(product => {
-//         product.title = updatedTitle;
-//         product.imageUrl = updatedImageUrl;
-//         product.description = updatedDescription;
-//         product.price = updatedPrice;
-//         return product.save()
-//     })
-//     .then(result => {
-//         console.log('updated product')
-//         res.redirect('/admin/products')
-//     })
-//     .catch(err => console.log(err))
-// }
+    const product = new Product(
+        updatedTitle, 
+        updatedImageUrl, 
+        updatedPrice, 
+        updatedDescription, 
+        new ObjectId(prodId))
+    product
+        .save()
+        .then(result => {
+            console.log('updated product')
+            res.redirect('/admin/products')
+        })
+        .catch(err => console.log(err))
+}
 
 
 // exports.postDeleteProduct = (req, res, next) => {
--- MongoDB/routes/admin.js
+++ MongoDB/routes/admin.js
@@ -15,8 +15,8 @@ router.post('/add-product',adminController.postAddProduct)
 //  /admin/products => GET
 router.get('/products', adminController.getAdminProducts)
 
-// router.get('/edit-product/:productId', adminController.getEditProduct)
-// router.post('/edit-product', adminController.postEditProduct)
+router.get('/edit-product/:productId', adminController.getEditProduct)
+router.post('/edit-product', adminController.postEditProduct)
 
 // router.post('/delete-product', adminController.postDeleteProduct)
 
